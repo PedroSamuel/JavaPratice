cf8f7cde7e8ea7712b269434c7a054c999e27a2f
Pedro Silva <pedrbsilva@gmail.com>
2018-06-08 13:58:04 +0100

    progress on java project1

--- Projecto1/src/io/altar/jseproject/textinterface/TextInterface.java
+++ Projecto1/src/io/altar/jseproject/textinterface/TextInterface.java
@@ -9,56 +9,102 @@ public class TextInterface {
 		
 		////    main        //////
 	public static void main(String[] args) {
-		firstPage();
+		firstMenu();
 	 
 	
 	}
 	
-	//////    first page    /////////
-	public static void firstPage(){
+	/////////////////////////////////////    first menu    //////////////////////////////
+	public static void firstMenu(){
 		
 		boolean valid = false;
 		while (!valid){
 		
-		System.out.println("Por favor selecione uma das seguintes opções:");
-		System.out.println("1)	Listar produtos");
-		System.out.println("2)	Listar prateleiras");
-		System.out.println("3)	Sair");
-		
-			String choice = scanner.nextLine();
+			System.out.println("Por favor selecione uma das seguintes opções:");
+			System.out.println("1)	Listar produtos");
+			System.out.println("2)	Listar prateleiras");
+			System.out.println("3)	Sair");
 			
-		try {	
-				int op = Integer.parseInt(choice);
-		} catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			continue;
-		}
-		
-		if (choice == 1){
-			System.out.print("ok");
-		}
-		
-	  
-//	     switch(choice){
-//     	case("1"):
-//     	case("2"):	
-//     	case("3"):
-//    		valid = true;
-//     		break;
-//    	default:
-//   		System.out.println("Introduza uma opcao valida");
-//    		break;	
-//	     		
-     //} 
-	    
+			int choice = scanner.nextInt();
+			
+			//voltar a experimentar com switch.
+			
+			//switch(choice)
+			
+			
+			
+			if (choice == 1){
+				listProducts();
+				valid = true;
+			} else if (choice == 2){
+				listShelfs();
+			} else if (choice == 3);
+				System.out.println("Obrigado pela visita.");
+				System.out.println("#####################");
+				valid = true;
+				
+
+				
 		}	     
- //scanner.close();
 
 
 
-		
-		
-		
 	}
+		//tentar voltar 
+	/////////////////////////////////////  Listar Produtos    ////////////////////////////
+	public static void listProducts(){
+		
+		boolean valid = false;
+		
+		while (!valid){
+			System.out.println("Por favor selecione uma das seguintes opções:");
+			System.out.println("1)	Criar novo produto");
+			System.out.println("2)	Editar um produto existente");
+			System.out.println("3)	Consultar os detalhes de um produto");
+			System.out.println("4)	Remover um produto");
+			System.out.println("5)	Voltar ao menu anterior");
+			int choice = scanner.nextInt();
+			if (choice == 1){
+				listProducts();
+				valid = true;
+			} else if (choice == 2){
+				listShelfs();
+			} else if (choice == 3);
+				firstMenu();
+				valid = true;
+		}	
+		     
+	}
+	
+	/////////////////////////////////////  Listar Prateleiras    ////////////////////////////
+	public static void listShelfs(){
 
+		boolean valid = false;
+		
+		while (!valid){
+			System.out.println("Por favor selecione uma das seguintes opções:");
+			System.out.println("1)	Criar nova prateleira");
+			System.out.println("2)	Editar uma prateleira existente");
+			System.out.println("3)	Consultar os detalhes de uma prateleira");
+			System.out.println("4)	Remover uma prateleira");
+			System.out.println("5)	Voltar ao menu anterior");
+			int choice = scanner.nextInt();
+			
+			if (choice == 1){
+				listProducts();
+				valid = true;
+			} else if (choice == 2){
+				listShelfs();
+			} else if (choice == 3);
+				firstMenu();
+				valid = true;
+		}	
+
+	}
+	
+	
+	
+	
+	
+		//scanner.close(); 
 }
